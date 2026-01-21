ee1be737dc00c8ade66b1d75f7f9cdb790b53e95
Théo <theo.lazariss@gmail.com>
2026-01-21 20:33:56 -0300

    chore(mock): add dashboard mock data

---
+++ resources/js/Pages/Tenant/Mocks/DashboardMock.js
@@ -0,0 +1,11 @@
+export const dashboardMock = {
+  totalContracts: 85,
+  activeContracts: 62,
+  expiring30Days: 7,
+  expiring60Days: 12,
+  pipeline: {
+    draft: 30,
+    review: 15,
+    signed: 55,
+  },
+}
