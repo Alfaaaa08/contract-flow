89020002f05e968354681185921a0ebc9085816b
Théo <theo.lazariss@gmail.com>
2026-02-21 18:28:38 -0300

    feat(api): configure API routes and JWT middleware aliases

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
+        api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
@@ -43,6 +43,11 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->api(prepend: [
             \App\Http\Middleware\InitializeTenancyFromJWT::class,
+        ]);
+
+        $middleware->alias([
+            'jwt.auth' => \Tymon\JWTAuth\Http\Middleware\Authenticate::class,
+            'jwt.refresh' => \Tymon\JWTAuth\Http\Middleware\RefreshToken::class,
         ]);
     })
 
