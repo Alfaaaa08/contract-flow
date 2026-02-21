affc8760f034859df58d9787ad723a0f14c9274e
Théo <theo.lazariss@gmail.com>
2026-02-21 15:40:34 -0300

    feat(api): create multi-tenant middleware for JWT

--- app/Http/Middleware/InitializeTenancyFromJWT.php
+++ app/Http/Middleware/InitializeTenancyFromJWT.php
@@ -5,26 +5,30 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use App\Models\Tenant;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
-class InitializeTenancyFromJWT
-{
-    public function handle(Request $request, Closure $next)
-    {
-        if (!auth('api')->check()) {
-            return $next($request);
-        }
+class InitializeTenancyFromJWT {
+    public function handle(Request $request, Closure $next) {
+        try {
+            $token = JWTAuth::getToken();
 
-        /** @var \Tymon\JWTAuth\JWTGuard $guard */
-        $guard = auth('api');
-
-        $tenantId = $guard->payload()->get('tenant_id');
-
-        if ($tenantId) {
-            $tenant = Tenant::find($tenantId);
-            
-            if ($tenant) {
-                tenancy()->initialize($tenant);
+            if (!$token) {
+                return $next($request);
             }
+
+            $payload = JWTAuth::getPayload($token);
+
+            $tenantId = $payload->get('tenant_id');
+
+            if ($tenantId) {
+                $tenant = Tenant::find($tenantId);
+
+                if ($tenant) {
+                    tenancy()->initialize($tenant);
+                }
+            }
+        } catch (JWTException $e) {
         }
 
         return $next($request);
